8642a46fb4045ddfb84818f44f2f72e8c6b41c76
afluffysquirrel <afluffysquirrel@gmail.com>
2024-10-15 13:25:13 +0100

    fix: config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const AUTHOR_NAME = 'Nuno Marques'
-export const SITE_URL = 'https://design-code.tips'
-export const SITE_NAME = 'design-code.tips'
+export const AUTHOR_NAME = 'Chris Aldred'
+export const SITE_URL = 'https://decap-blog-example.netlify.app'
+export const SITE_NAME = 'decap-blog-example'
 export const SHOW_PER_PAGE = 10
--- next-seo.config.js
+++ next-seo.config.js
@@ -2,7 +2,7 @@ export default {
   openGraph: {
     type: 'website',
     locale: 'en',
-    url: 'https://design-code.tips',
-    site_name: 'design-code.tips',
+    url: 'https://decap-blog-example.netlify.app',
+    site_name: 'decap-blog-example',
   },
-};
+}
